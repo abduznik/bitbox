a19fce64067c200a68a0075fe6e073c3f130a38a
PurpleSwtr <sergeenko.mihail@yandex.ru>
2026-07-14 21:15:31 +0300

    [tool] tool/reading_time (#163)

--- tools/reading_time.py
+++ tools/reading_time.py
@@ -4,16 +4,10 @@
 # example: reading_time "400 words of text here" -> "2"
 
 
-import sys
-
-
 def run(*args) -> str:
-    text = args[0] if args else sys.stdin.read()
-
+    text = args[0]
     word_count = len(text.strip().split())
-
     if word_count == 0:
         return "0"
-
     minutes = (word_count + 100) // 200
     return str(minutes)
