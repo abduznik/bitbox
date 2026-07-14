8f9e875e9160b910850943c011150bdc9fca61ac
Ayesha <faquihayesha@gmail.com>
2026-07-14 23:01:03 +0530

    Added changes recommended

--- tools/hex_to_decimal.py
+++ tools/hex_to_decimal.py
@@ -5,16 +5,7 @@
 
 
 def run(*args) -> str:
-    text = list(args[0])
-    convert=[]
-    alpha={"a":10, "b":11, "c":12, "d":13, "e":14, "f":15}
-    j=0
-    for i in range(len(text)-1,-1,-1):
-        try:
-            text[i]=int(text[i])
-        except:
-            text[i]=alpha.get(text[i])
-        finally:
-            convert.append(int(text[i])*16**(j))
-            j+=1
-    return str(sum(convert))
+    try:
+        return str(int(args[0], 16))
+    except (ValueError, IndexError):
+        return f"Error: '{args[0]}' is not a valid hexadecimal string"
