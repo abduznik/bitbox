120ccaca5e2be7fbe12601f88ca504f59bd9b511
Vlad Cacenschi <cacenschivlad@gmail.com>
2026-08-24 13:30:50 +0300

    binary_to_octal tool errors handled

--- tools/binary_to_octal.py
+++ tools/binary_to_octal.py
@@ -4,5 +4,20 @@
 # example: binary_to_octal "1010" -> "12"
 
 
-def run(binary: str) -> str:
-    return f"{int(binary, 2):o}"
+def run(*args) -> str:
+    try:
+        binary = args[0]
+    except IndexError:
+        return "Error: Function must have argument"
+    
+    if type(binary) != str:
+        return "Error: Argument must be of type str"
+    
+    try:
+        octal = f"{int(binary, 2):o}"
+        return octal
+    except ValueError:
+        return f"Error: '{binary}' is not a valid binary string"
+    
+if __name__ == "__main__":
+    print(run("\102"))
