d86798e638dc545df87176d3d3b4913bdaf174dc
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-09-13 18:43:19 +0530

    [tool] count_uppercase (#429)
    * Add count_uppercase tool
    
    * fix: validate count_uppercase arguments

---
+++ tools/count_uppercase.py
@@ -0,0 +1,19 @@
+# tool: count_uppercase
+# description: Count the number of upper case letters in a String
+# author: @ShauryaPrakashVerma
+# example: count_uppercase "Hello World" -> "2"
+
+
+def run(*args) -> str:
+    
+    if len(args) < 1:
+        return "Error: requires a string argument"
+    
+    string = args[0]
+    count = 0
+    
+    for char in string:
+        if char.isupper():
+            count += 1
+            
+    return str(count)
