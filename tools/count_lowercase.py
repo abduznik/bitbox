32c27dfff470fb77af63ab7fc47a11ae10161241
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-09-13 18:43:16 +0530

    [tool] count_lowercase (#430)
    * Add count_lowercase tool
    
    * Add count_lowercase tool
    
    * fix: validate count_lowercase arguments

---
+++ tools/count_lowercase.py
@@ -0,0 +1,19 @@
+# tool: count_lowercase
+# description: Count the number of lower case letters in a String
+# author: @ShauryaPrakashVerma
+# example: count_lowercase "Hello World" -> "8"
+
+
+def run(*args) -> str:
+    
+    if len(args) < 1:
+        return "Error: requires a string argument"
+    
+    string = args[0]
+    count = 0
+    
+    for char in string:
+        if char.islower():
+            count += 1
+            
+    return str(count)
